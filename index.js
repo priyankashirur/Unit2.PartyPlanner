fd9adcc1f3d010dd107bcff8de971745674eada8
Priyanka Shirur <pshirur323@gmail.com>
2024-11-25 08:38:00 -0600

    syntax in renderParties

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/events";
-const cohort = "2410-FTB-ET-WEB-FT";
+const COHORT = "2410-FTB-ET-WEB-FT";
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
 //need to create a state to hold the parties data:
 
 const state = {
-    parties: [];
-}
+  parties: [],
+};
 
 //Step 1: Get party data from the API using fetch.
 // - create an async function
@@ -14,6 +14,7 @@ async function getParties(params) {
   try {
     const response = await fetch(API_URL); //grab API database
     const parties = await response.json(); //turn it into json because everything is a string initially
+    console.log("API Response:", parties); // Log the response
     state.parties = parties; // store parties data in the state object
   } catch (error) {
     console.log(error);
@@ -30,7 +31,7 @@ function renderParties() {
   }
 
   const eventCards = state.parties.map((party) => {
-    const party = document.createElement("li");
+    const card = document.createElement("li");
     card.innerHTML = `
        <h2>${party.name}</h2>
        <img src="${party.imageUrl}" alt="${party.name}" />
@@ -39,10 +40,12 @@ function renderParties() {
     return card;
   });
 
-  artistList.replaceChildren(...artistCards);
+  partyList.replaceChildren(...eventCards);
 }
 
 async function render() {
   await getParties();
   renderParties();
 }
+
+render();
